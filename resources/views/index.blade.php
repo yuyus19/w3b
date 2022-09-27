f00847f267ce6080b08b114138ddf714b8db515c
Yusuf Yusuf <56111176+yuyus19@users.noreply.github.com>
2022-09-27 16:53:27 +0200

    n
    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,17 +12,26 @@
 <body>
     <nav>
         <div class="container">
+<<<<<<< HEAD
             <a id="logo" href="resources\views\index.blade.php">
+=======
+<<<<<<< HEAD
+            <a id="logo" href="http://127.0.0.1:8000/">
+>>>>>>> parent of 092ca5b (Revert "Merge branch 'main' of https://github.com/yuyus19/w3b into main")
                 <h2>MyPC</h2>
+=======
+            <a id="logo" href="resources\views\index.blade.php">
+                <h2>MyPC</h2></a>
+>>>>>>> 95b562d6d4337ca7e559093dfe26f21a726c480b
                 <h3>WebStore</h3>
 
-            </a>
             <div class="cart">
                 <span>In cart: </span><span id="cart-count"> 0</span>
             </div>
         </div>
     </nav>
     <section class="container">
+<<<<<<< HEAD
     <main>
         <div class="pc">
             <section class ="column" style="background-color:blue;"><h2>The GigaChad 20000</h2><p>Data</p></section>
@@ -31,6 +40,59 @@
         <div class ="column" style="background-color:red;"><h2>The PlebBoi</h2><p>Data</p></div>-->    
     </div>
     </main>
+=======
+<<<<<<< HEAD
+        <main>
+            <div class="pc">
+                <section class="column" style="background-color:blue;">
+                    <h2>The GigaChad 20000</h2>
+                    <p>Data</p>
+                    <img src="/img/pc1.png" alt="gigaChad image Not found">
+
+                </section>
+                <section class="column" style="background-color:red;">
+                    <h2>The PlebBoi</h2>
+                    <p>Data</p>
+                    <img src="\img\pc2.png" alt="pleboi image not found">
+                </section>
+                <!--<div class ="column" style="background-color:blue;"><h2>The GigaChad 20000</h2><p>Data</p></div>
+        <div class ="column" style="background-color:red;"><h2>The PlebBoi</h2><p>Data</p></div>-->
+            </div>
+        </main>
+=======
+    <main>
+        <!--<div class="pc"><h2>The GigaChad 20000</h2></div>
+        <div class="pc"><h2>The PlebBoi</h2></div>-->
+        <div class="pc">
+            <section class ="column" style="background-color:blue;">
+        
+            <h2>The GigaChad 20000</h2>
+            <p>The GigaChad 20000 is the perfect machine for all of your gaming needs, with RGB that will literally make you puke.</p>
+            <ul>
+                <li><b>1000</b> GigaWatt Processor</li>
+                <li>4x Undimmed Memoryless RAM</li>
+                <li>Supports more than one monitor</li>
+            </ul>
+        </section>
+
+        </div>
+        <div class="pc">
+            <section class ="column" style="background-color:red;">
+            
+            <h2>The PlebBoi</h2>
+            <p>The only computer made for betas by betas. The ideal computer that comes with a 2 year simp warranty.</p>
+            <ul>
+                <li>RGB to satisfy all your needs½</li>
+                <li>1034 KB Virtual <b>Attachable</b> RAM</li>
+                <li>8096 KBit Ethernet porth</li>
+                <li>Cheetos resistant keyboard</li>
+                <li><b>$5</b> OnlyFans gift card included</li>
+            </ul>
+        </section>
+        </div>
+    </main>
+>>>>>>> 95b562d6d4337ca7e559093dfe26f21a726c480b
+>>>>>>> parent of 092ca5b (Revert "Merge branch 'main' of https://github.com/yuyus19/w3b into main")
     </section>
 </body> 
 
